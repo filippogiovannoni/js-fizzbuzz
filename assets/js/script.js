49eedb682b10bbd2dd39e1efd8adad6a383af3d3
filippogiovannoni <filippogiovannonii@gmail.com>
2023-11-27 17:52:44 +0100

    fix script

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,13 +7,13 @@
 //Ciclo for che stampa numeri da 1 a 100
 for (let n = 1; n <= 100; n++) {
 
-    const boxElement = document.createElement('div')
+    const boxElement = document.createElement('div');
 
-    boxElement.classList.add('box')
+    boxElement.classList.add('box');
 
-    const containerEl = document.querySelector('.container')
+    const containerEl = document.querySelector('.container');
 
-    containerEl.append(boxElement)
+    containerEl.append(boxElement);
 
     //Verifico se sono multipli tramite l'operatore modulo (%)
 
@@ -21,29 +21,29 @@ for (let n = 1; n <= 100; n++) {
     if (n % 3 == 0 && n % 5 == 0) {
         //Viene stampato in console FizzBuzz
         console.log('FizzBuzz');
-        boxElement.innerText = 'FizzBuzz'
-        boxElement.classList.add('box-fizzbuzz')
+        boxElement.innerText = 'FizzBuzz';
+        boxElement.classList.add('box-fizzbuzz');
     }
     //Se il numero della variabile n è multiplo di 3
     else if (n % 3 == 0) {
         //Viene stampato in console Fizz
         console.log('Fizz');
-        boxElement.innerText = 'Fizz'
-        boxElement.classList.add('box-fizz')
+        boxElement.innerText = 'Fizz';
+        boxElement.classList.add('box-fizz');
 
         //Se il numero della variabile n è multiplo di 5
     } else if (n % 5 == 0) {
         //Viene stampato in console Buzz
         console.log('Buzz');
-        boxElement.innerText = 'Buzz'
-        boxElement.classList.add('box-buzz')
+        boxElement.innerText = 'Buzz';
+        boxElement.classList.add('box-buzz');
     }
 
     //Altrimenti viene stampato il numero della variabile n
     else {
-        console.log(n)
-        boxElement.innerText = n
-        boxElement.classList.add('box-number')
+        console.log(n);
+        boxElement.innerText = n;
+        boxElement.classList.add('box-number');
     }
 }
 
